f3d24e839ad5d9ef74a383112ac30b9d9700398d
PedroPizarro <pedrin04pz@gmail.com>
2021-01-11 20:08:30 -0300

    Changed makefile to produce elf64 in dwarf debugging format. New lib version

--- AssemblyFiles/src/lib.asm
+++ AssemblyFiles/src/lib.asm
@@ -1,7 +1,8 @@
 global _start 
 
-section .data
-message:
+; dados para testes
+section .data 
+message: db "hello, world", 10, 0
 
 section .text 
 
@@ -9,65 +10,55 @@ section .text
 exit:
     ; O código de saída deve estar no acumulador
     mov rdi, rax       ; passa o código de saída no acumulador para o descritor de saída (RDI)
-    mov rax, 60        ; número da syscall de "exit"  
+    mov rax, 60        ; número da syscall "exit"  
     syscall 
 ;***
 
 ; Função que aceita um ponteiro para a string e devolve seu tamanho
 string_length:
-    ; Incialização da função 
-    ; O ponteiro deve ter sido previamente alocado no registrador RDI
-    push r13            ; salva o valor de r13 na stack
-    xor r13,r13         ; zera o valor em r13 para iniciar a contagem
+    ; O ponteiro previamente alocado em "rdi"
+    xor rax, rax
 .loop:
-    cmp byte[r13 + rdi], 0 ; confirma se não chegou no finalizador nulo
-                           ; byte [] explicita que estamos com um ponteiro que aponta para um dado de tamanho de 1 byte 
+    cmp byte[rdi + rax], 0 ; confirma se não chegou no finalizador nulo
+                           ; "byte" explicita o ponteiro de um dado de 1 byte.  
     je .end                ; caso chegou no caractere nulo, finaliza a contagem (je  = jz => jump se a flag de zero for setada)
-    inc r13                ; se não, incrementa o registrador para contar o próximo caractere
+    inc rax                ; se não, incrementa o registrador para contar o próximo caractere
     jmp .loop              
 .end:
-    mov rax, r13           ; passa o tamanho da string pro acumulador
-    pop r13                ; resgata o valor inicial de r13
-    call exit              ; chama a função de saída -> retorna o tamanho da string 
+    ret                    ; retorna com RAX armazenando o tamanho da string
 ;***
 
 ; Função que aceita um ponteiro de string com terminação nula e exibe-a em stdout
-print_string:
-    push r13
-    push r15
-    xor r13,r13
-    mov rax, 1
-    mov rdi, 1          
-.loop:
-    cmp byte[r13 + r15], 0  
-    je .end
-    inc r13
-    jmp .loop
-.end:
-    mov rsi, r15
-    mov rdx, r13
-    pop r15
-    pop r13
+print_string: 
+    ; O ponteiro previamente alocado em "rdi"
+    push rdi               ; salva o endereço da mensagem na pilha
+    call string_length
+    pop rsi                ; pega o endereço salvo por "rdi" na pilha
+    mov rdx, rax           ; o número de bytes que deve ser escrito (tamanho da string)
+    mov rax, 1             ; número da syscall "write"
+    mov rdi, 1             ; descritor de stdout (terminal)
     syscall
-    xor rax, rax
-    call exit 
+    ret 
 ;***
 
 ; Função que aceita um caractere diretamente como argumento e exibe-o em stdout
 print_char:
-    mov [message], rax
-    mov rax, 1          ; número da syscall de "write"
-    mov rdi, 1          ; descritor (nesse caso indica que vai escrever em "stdout")
-    mov rsi, message  ; onde está o char, nesse caso pega-se o endereço do registrador que está armazenado
-    mov rdx, 1          ; número de bytes que vai ser escrito
-    xor rax, rax
-    call exit 
+    push rdi               ; poe o código do caracter na pilha
+    mov rsi, rsp           ; passa o endereço do código do caracter para rsi
+    call print_string
+    pop rdi                ; retorna o código do caracter para rdi (boa prática!) 
+    ret
+;***
 
-;**
-;* MAIN
-;**
+; Exibe o caractere "\n"
+print_newline:
+    mov rdi, 0xA
+    
+;
+; MAIN
+;
 _start:
-    mov rax, 0xA
-    call print_char
+    mov rax, 0
+    call exit 
     
 
